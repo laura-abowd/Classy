59061b13dc45d077f8cce7ff30a960c76c4035eb
Naoto Suzuki <naoto.n.suzuki@gmail.com>
2019-11-19 13:31:19 +0900

    updated algorithms

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,35 +1,18 @@
 class StudentsController < ApplicationController
+
   def index
     @students = Student.all
     @student = Student.first
+    # @mystudents = Student.joins(:classroom_enrollments, :classrooms).where(teacher_id: current_teacher.id)
 
   end
 
   def update
-  end
-
-
-  def sort
-# NS I'm using this sort method to house the algorithm for now
-
-
-
-
-
-#put everyone together
-
-#create classroom
-
-#to asign -creating classroom_enrollment
-
-
-
-
-
-
-
 
   end
+
+
+
 
 end
 
--- app/models/grade.rb
+++ app/models/grade.rb
@@ -2,4 +2,6 @@ class Grade < ApplicationRecord
   has_many :classrooms
   has_many :students
   has_many :teachers
+
+
 end
