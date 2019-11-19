2900b370d0b8e0c6b6938bc165e58219ae1d516b
Naoto Suzuki <naoto.n.suzuki@gmail.com>
2019-11-19 15:48:25 +0900

    updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,17 @@
 class PagesController < ApplicationController
+
+  skip_before_action :authenticate_user!, only: :home
+
   def home
 
   end
 
+
 # NS I'm using this sort method to house the algorithm for now
 
-  def sorttest
-    doug = Teacher.find_by(first_name: 'Doug')
-    secondgrade = Grade.find_by(level: 2)
-    10.times do Classroom.create!(teacher: doug, grade: secondgrade)
-    end
-    raise
-  end
+  # def sorttest
+
+  # end
 
   def group_students_by_grade
     #put all students in the same grade together
@@ -19,6 +19,37 @@ class PagesController < ApplicationController
   end
 
   def creating_classes
+    # create fives classes and place them in variables
+    hold = Teacher.find_by(first_name: 'hold')
+    secondgrade = Grade.find_by(level: 2)
+    5.times do Classroom.create!(teacher: hold, grade: secondgrade)
+    end
+    # classone = Classroom.all[-1]
+    # classtwo = Classroom.all[-2]
+    # classthree = Classroom.all[-3]
+    # classfour = Classroom.all[-4]
+    # classfive = Classroom.all[-5]
+
+    # OR
+
+    # classone = []
+    # classtwo = []
+    # classthree = []
+    # classfour = []
+    # classfive = []
+
+  end
+
+  def sorttest
+    # loop through students from group_students_by_grade into classrooms from creating_classes
+    group_students_by_grade
+    @students.each_with_index do |student, index|
+      if index % 1 = 0
+    raise
+
+
+
+
 
   end
 
@@ -27,9 +58,7 @@ class PagesController < ApplicationController
 
 
 
-#create five classrooms
 
-#start assigning
 
 
 
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,6 @@
 class Student < ApplicationRecord
   belongs_to :grade
+
   has_many :classroom_enrollments
   has_many :do_not_places
 
