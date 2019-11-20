a337506af642dda5a16254685ec08fe1b60b31b7
Kloey Yap <kloeyyap@gmail.com>
2019-11-20 19:21:39 +0900

    dropdown is actually working omg

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,26 @@
 //= require rails-ujs
 //= require_tree .
+// document.querySelector('#view-dropdown').addEventListener('click', () => {
+//   document.querySelector('.dropdown-card').classList.toggle('active')
+// })
+
+// document.querySelectorAll('.yellow-btn').addEventListener("click", () => {
+//   document.querySelectorAll('.student-card-container').classList.toggle('active')
+// })
+
+// NEXT FEATURE
+// document.querySelectorAll('.yellow-btn').forEach( (button) => {
+//   button.addEventListener("click", () => {
+//     document.querySelectorAll('.student-card-container').forEach( (card) => {
+//       card.classList.toggle('active')
+//     })
+//   });
+// })
+
+
+document.querySelectorAll('.student-card-container').forEach( (card) => {
+  button = card.querySelector('.yellow-btn')
+  button.addEventListener("click", () => {
+    card.classList.toggle('active')
+  });
+})
