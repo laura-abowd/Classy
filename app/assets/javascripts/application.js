25eb5a0b4b547f7a86183b78a5f263f05a8719f5
Kloey Yap <kloeyyap@gmail.com>
2019-11-21 19:06:57 +0900

    checkbox ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,16 +19,38 @@
 
 
 document.querySelectorAll('.student-card-container').forEach( (card) => {
-  button = card.querySelector('.yellow-btn')
+  button = card.querySelector('.fa-chevron-down');
   button.addEventListener("click", () => {
     card.classList.toggle('active')
   });
 })
 
 
-const checkAll = document.querySelectorAll('.checkbox-icon i');
-checkAll.forEach( (button) => {
-    button.addEventListener("click", (event) => {
-        event.currentTarget.classList.toggle("active")
-    });
-})
+// const checkAll = document.querySelectorAll('.checkbox-icon i');
+// checkAll.forEach( (button) => {
+//     button.addEventListener("click", (event) => {
+//         event.currentTarget.classList.toggle("active");
+//     });
+//   if (button.classList.contains('active')) {
+
+//   }
+
+// })
+
+
+// if (document.querySelector('.checkbox-icon i').classList.contains('active')) {
+//   document.getElementById('student_esl').click();
+// } else { console.log('no') };
+
+// if (document.querySelector('.checkbox-icon i').classList.contains('active')) {
+//   document.getElementById('student_gifted_talented').click();
+// } else { console.log('no') };
+
+// if (document.querySelector('.checkbox-icon i').classList.contains('active')) {
+//   document.getElementById('student_special_education').click();
+// } else { console.log('no') };
+
+// if (document.querySelector('.checkbox-icon i').classList.contains('active')) {
+//   document.getElementById('student_medical_alert').click();
+// } else { console.log('no') };
+
