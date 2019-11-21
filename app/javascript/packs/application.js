a3ee5d4adb7d5279c44ed44012f1007aa2852121
Laura Abowd <laura.abowd@gmail.com>
2019-11-21 12:12:39 +0900

    fixed javascript syntax

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@ import "bootstrap";
 window.onscroll = function() {myFunction()};
 
 // Get the header
-var header = document.getElementById("myHeader");
+var header = document.getElementById("student-header");
 
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
