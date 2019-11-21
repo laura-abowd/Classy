5702f9fc49c836e88d82109316018c1d02750ced
Kloey Yap <kloeyyap@gmail.com>
2019-11-21 19:58:09 +0900

    extra feature

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,21 +1,13 @@
 //= require rails-ujs
 //= require_tree .
-// document.querySelector('#view-dropdown').addEventListener('click', () => {
-//   document.querySelector('.dropdown-card').classList.toggle('active')
-// })
 
-// document.querySelectorAll('.yellow-btn').addEventListener("click", () => {
-//   document.querySelectorAll('.student-card-container').classList.toggle('active')
-// })
-
-// NEXT FEATURE
-// document.querySelectorAll('.yellow-btn').forEach( (button) => {
-//   button.addEventListener("click", () => {
-//     document.querySelectorAll('.student-card-container').forEach( (card) => {
-//       card.classList.toggle('active')
-//     })
-//   });
-// })
+document.querySelectorAll('.classy-sticky-button .yellow-btn').forEach( (button) => {
+  button.addEventListener("click", () => {
+    document.querySelectorAll('.student-card-container').forEach( (card) => {
+      card.classList.toggle('active')
+    })
+  });
+})
 
 
 document.querySelectorAll('.student-card-container').forEach( (card) => {
@@ -25,32 +17,4 @@ document.querySelectorAll('.student-card-container').forEach( (card) => {
   });
 })
 
-
-// const checkAll = document.querySelectorAll('.checkbox-icon i');
-// checkAll.forEach( (button) => {
-//     button.addEventListener("click", (event) => {
-//         event.currentTarget.classList.toggle("active");
-//     });
-//   if (button.classList.contains('active')) {
-
-//   }
-
-// })
-
-
-// if (document.querySelector('.checkbox-icon i').classList.contains('active')) {
-//   document.getElementById('student_esl').click();
-// } else { console.log('no') };
-
-// if (document.querySelector('.checkbox-icon i').classList.contains('active')) {
-//   document.getElementById('student_gifted_talented').click();
-// } else { console.log('no') };
-
-// if (document.querySelector('.checkbox-icon i').classList.contains('active')) {
-//   document.getElementById('student_special_education').click();
-// } else { console.log('no') };
-
-// if (document.querySelector('.checkbox-icon i').classList.contains('active')) {
-//   document.getElementById('student_medical_alert').click();
-// } else { console.log('no') };
 
