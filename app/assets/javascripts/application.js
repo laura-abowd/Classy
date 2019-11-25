ef6bec0706726187d52f1bf8ebcf9bbe93f9b16b
Kloey Yap <kloeyyap@gmail.com>
2019-11-25 21:44:54 +0900

    fixed opening and closing all cards

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,13 +1,25 @@
 //= require_tree .
 
-document.querySelectorAll('.show-notes-btn .expand-view').forEach( (button) => {
-  button.addEventListener("click", () => {
+// if we push show all, look through each card and if it doesn't have active1, add it
+
+
+const showAllButton = document.querySelector('.show-notes-btn .expand-view')
+
+showAllButton.addEventListener("click", () => {
     document.querySelectorAll('.student-card-container').forEach( (card) => {
-      card.classList.toggle('active1')
-      console.log(card)
-    })
-  });
-})
+      if (showAllButton.classList.contains('all-closed')) {
+        card.classList.remove('active1');
+        showAllButton.innerText = "Show All Notes"
+      }
+      else {
+        console.log('firing')
+        card.classList.add('active1')
+        showAllButton.innerText = "Hide All Notes"
+      }
+    });
+    showAllButton.classList.toggle('all-closed');
+});
+
 
 
 document.querySelectorAll('.student-card-container').forEach( (card) => {
