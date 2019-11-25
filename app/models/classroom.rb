b5621861b9ec783cff7b06a3d4e5a5dd044869cb
Naoto Suzuki <naoto.n.suzuki@gmail.com>
2019-11-25 18:09:15 +0900

    view loop complete

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -2,7 +2,7 @@ class Classroom < ApplicationRecord
   belongs_to :teacher
   belongs_to :grade
   has_many :classroom_enrollments
-  has_many :students, through: :classroom_enrollments
+  has_many :students, through: :classroom_enrollments, source: :student
 
 
   # validates :year,  presence: true
