802cbdd6e10499e1933e131292b8f1a50f39d803
Laura Abowd <laura.abowd@gmail.com>
2019-11-25 21:37:10 +0900

    added stimulus and also removed js sticky banner code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,32 +1,20 @@
-
 import "bootstrap";
 import Rails from '@rails/ujs';
 import { submitOnEvent } from '../components/submitOnEvent.js';
-import { initSortable } from '../plugins/init_sortable';
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+// import { initSortable } from '../plugins/init_sortable';
 
 Rails.start();
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
 
-// Get the header
-const header = document.getElementById("header-container-sticky");
-
-
-// Get the offset position of the navbar
-const sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-const myFunction = () => {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
 
 submitOnEvent();
 // const list = document.querySelector('#results');
-initSortable();
+// initSortable();
 // myFunction();
 
+
+const application = Application.start()
+const context = require.context("../controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
