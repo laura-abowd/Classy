c436731d9ec5c2f4db1c5fa150c49996ec50bc93
Laura Abowd <laura.abowd@gmail.com>
2019-11-26 00:33:34 +0900

    small_student cards are now draggable

--- app/javascript/controllers/studentClassroom_controller.js
+++ app/javascript/controllers/studentClassroom_controller.js
@@ -4,11 +4,24 @@ export default class extends Controller {
   static targets = ['myText']
 
   connect() {
-    console.log("Connected:", this.element);
+    console.log(this.contentTarget);
   }
-
-  changeText() {
-    this.myTextTarget.innerText = "Yeahh! Stimulus!"
+  // changeText() {
+  //   this.myTextTarget.innerText = "Yeahh! Stimulus!"
+  // }
+    studentDragHandler(event) {
+  // console.log("I'm dragging!");
+    event.dataTransfer.setData("text/plain", event.target.id);
+    event.dropEffect = "move";
   }
 
 }
+
+
+
+ // playerDragHandler(event) {
+ //    // console.log("I'm dragging!");
+ //    // event.preventDefault();
+ //    event.dataTransfer.setData("text/plain", event.target.id);
+ //    event.dropEffect = "move";
+ //  }
