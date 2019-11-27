52003931536626582bde265f02b8f6eecc9d47eb
Kloey Yap <kloeyyap@gmail.com>
2019-11-27 20:58:33 +0900

    Ajax works. Me done

--- app/javascript/controllers/studentClassroom_controller.js
+++ app/javascript/controllers/studentClassroom_controller.js
@@ -24,9 +24,37 @@ export default class extends Controller {
     // Update classroom ID with dropzone classroom
     enrollmentForm.querySelector('#classroom_enrollment_classroom_id').value = container.dataset.classroomId
 
+    // Change the teacher class
+    // let studentClass = studentCard.className
+    // const nextStudentClass = container.dataset/.*-(.)/.classroomId
+    // // console.log(studentClass.match(/(.*-)(.)/)[1] + nextStudentClass)
+    // studentCard.className = studentClass.match(/(.*-)(.)/)[1] + nextStudentClass
 
     // submit that form
     Rails.fire(enrollmentForm, 'submit')
+
+
+
+    // Reload the student number
+    // 0. Create an dynamic and unique id of teacher in the HTML
+
+    // 1. Get elements of the student number of the each teacher
+    // let studentNumberOfTeachers = document.querySelectorAll('.ajax-teacher');
+    // let teacherList = []
+    // studentNumberOfTeachers.forEach((studentNumberOfTeacher) => {
+    //   teacherList.push(studentNumberOfTeacher.id.match()[1])
+    // })
+    // // console.log(teacherList)
+
+    // // 2. Get the latest student number by counting the number of the DOM
+    // let studentNumbers = []
+    // teacherList.forEach((teacher) => {
+    //   studentNumbers.push(document.querySelectorAll(`.teacher-${teacher}`).length)
+    // })
+    // console.log(studentNumbers)
+
+    // 3. Overwrite the number of student with new studentNumbers
+
   }
 
   student_dragover_handler(event) {
