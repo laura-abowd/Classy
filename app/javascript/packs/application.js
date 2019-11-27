4b00cdde1a992594692906d8b9e7310dc8eaeb79
Laura Abowd <laura.abowd@gmail.com>
2019-11-27 13:02:25 +0900

    added popover to view notes omn student_cards

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,6 @@ import Rails from '@rails/ujs';
 import { submitOnEvent } from '../components/submitOnEvent.js';
 
 import { initSelect2 } from '../components/init_select2.js';
-
-
-
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 // import { initSortable } from '../plugins/init_sortable';
@@ -16,10 +13,12 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 Rails.start();
 
 
-
 initSelect2();
 submitOnEvent();
 
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
 
 
 
